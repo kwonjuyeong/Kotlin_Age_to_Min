a030f79427955467365310d53dd2f0e1ef73f066
Kwon <20161527@vision.hoseo.edu>
2022-03-16 18:21:05 +0900

    Third test

--- app/src/main/java/com/example/minuite_age/MainActivity.kt
+++ app/src/main/java/com/example/minuite_age/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.minuite_age
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+
+            clickDatePicker()
+
+        }
+    }
+
+    fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,{
+            view,year,month,dayOfMonth ->
+
+            Toast.makeText(this, "picker works", Toast.LENGTH_LONG).show()
+
+        }, year,month, day).show()
+
     }
 }
